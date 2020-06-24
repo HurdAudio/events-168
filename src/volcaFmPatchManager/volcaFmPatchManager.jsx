c085ef8165ca0531fd7c71c69d53a4d38b8bc01c
hurdaudio <hurdaudio@gmail.com>
2020-06-23 21:52:08 -0600

    patch list and bank navigation work

--- src/volcaFmPatchManager/volcaFmPatchManager.jsx
+++ src/volcaFmPatchManager/volcaFmPatchManager.jsx
@@ -12,6 +12,7 @@ import {
 import midi5pin from '../img/midi5pin.svg';
 import './volcaFmPatchManager.style.jana.css';
 import axios from 'axios';
+import uuid4 from 'uuid4';
 
 function VolcaFmPatchManager(user, banks) {
     
@@ -41,14 +42,90 @@ function VolcaFmPatchManager(user, banks) {
     const [loadPatchUuid, setLoadPatchUuid] = useState(null);
     const [userPatches, setUserPatches] = useState([]);
     const [currentSpinner, setCurrentSpinner] = useState(janaSpinner);
+    const [selectedPatchPatch, setSelectedPatchPatch] = useState('');
     const [availableInputs, setAvailableInputs] = useState([]);
     const [availableOutputs, setAvailableOutputs] = useState([]);
     const [currentOutput, setCurrentOutput] = useState(0);
     const [currentMidiChannel, setCurrentMidiChannel] = useState(0);
     const [midiImage, setMidiImage] = useState(midi5pin);
     const [patchCollection, setPatchCollection] = useState({
+        banks: [
+            {
+                uuid: '76977154-b571-4937-86ab-12a3d062495e',
+                name: 'pads',
+                patches: []
+            },
+            {
+                uuid: 'ce0c3188-a670-4a3e-9b1a-50f9206e7603',
+                name: 'leads',
+                patches: []
+            },
+            {
+                uuid: '6a1a66bd-2d20-484a-ae74-9a40c9aafa5e',
+                name: 'percussion',
+                patches: []
+            }
+        ],
         name: 'collection 1'
     });
+    const [currentEditBank, setCurrentEditBank] = useState(patchCollection.banks[0].uuid);
+    
+    const makePatchPatchCurrent = (val) => {
+        setSelectedPatchPatch(val);
+    }
+    
+    const changeBankEdit = (val) => {
+        setCurrentEditBank(val);
+    }
+    
+    const addNewBank = () => {
+        let deepCopy = {...patchCollection};
+        const id = uuid4();
+        
+        deepCopy.banks.push({
+            uuid: id,
+            name: 'new',
+            patches: []
+        });
+        
+        setPatchCollection(deepCopy);
+        setCurrentEditBank(id);
+    }
+    
+    const updateBankName = (val) => {
+        let deepCopy = {...patchCollection};
+        let index = null;
+        
+        for (let i = 0; i < deepCopy.banks.length; i++) {
+            if (deepCopy.banks[i].uuid === currentEditBank) {
+                index = i;
+            }
+        }
+        if (index === null) {
+            alert('ERROR: Current Bank Set not found');
+            return;
+        }
+        deepCopy.banks[index].name = val;
+        setPatchCollection(deepCopy);
+    }
+    
+    const deleteCurrentBank = () => {
+        let deepCopy = {...patchCollection};
+        let index = null;
+        
+        for (let i = 0; i < deepCopy.banks.length; i++) {
+            if (deepCopy.banks[i].uuid === currentEditBank) {
+                index = i;
+            }
+        }
+        if (index === null) {
+            alert('ERROR: Current Bank Set not found');
+            return;
+        }
+        deepCopy.banks.splice(index, 1);
+        setCurrentEditBank(deepCopy.banks[0].uuid);
+        setPatchCollection(deepCopy);
+    }
     
     const patchCollectionNameUpdate = (val) => {
         let deepCopy = {...patchCollection};
@@ -56,6 +133,23 @@ function VolcaFmPatchManager(user, banks) {
         deepCopy.name = val;
         
         setPatchCollection(deepCopy);
+    }
+    
+    const updateUserPatches = () => {
+        axios.get(`/volca_fm_patches/byuser/${user.uuid}`)
+        .then(patchesData => {
+            const patches = patchesData.data.sort((a, b) => {
+                if (a.patch_name.toLowerCase() > b.patch_name.toLowerCase()) {
+                    return 1;
+                } else if (a.patch_name.toLowerCase() < b.patch_name.toLowerCase()) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            });
+            setUserPatches(patches);
+            console.log(userPatches);
+        });
     }
 
     const noteOnEvent = (key) => {
@@ -334,6 +428,17 @@ function VolcaFmPatchManager(user, banks) {
 
     initiateMidiAccess();
     
+    const getCurrentBankName = () => {
+        let index = null;
+        for (let i = 0; i < patchCollection.banks.length; i++) {
+            if (patchCollection.banks[i].uuid === currentEditBank) {
+                index = i;
+            }
+        }
+        
+        return patchCollection.banks[index].name;
+    }
+    
     
     
     return ( 
@@ -356,9 +461,53 @@ function VolcaFmPatchManager(user, banks) {
                     <button className={'volcaFmPatchManagerPanicButton' + volcaFmPatchManagerMonth}
                         onClick={() => panic()}>panic!</button>
                     <div className={'volcaFmPatchManagerSidebarManager' + volcaFmPatchManagerMonth}></div>
-                    <div className={'volcaFmPatchManagerUserPatchesListDiv' + volcaFmPatchManagerMonth}></div>
+                    <div className={'volcaFmPatchManagerUserPatchesListDiv' + volcaFmPatchManagerMonth}>
+                        <div className={'volcaFmPatchManagerUserPatchesListContainer' + volcaFmPatchManagerMonth}>
+                            <div className={'volcaFmPatchManagerUserPatchesList' + volcaFmPatchManagerMonth}></div>
+                            <div className={'volcaFmPatchManagerUserPatchListRealDiv' + volcaFmPatchManagerMonth}>
+                                {userPatches.map(patch => 
+                                    <p className={'volcaFmPatchManagerPatchNameDisplay' + (selectedPatchPatch === patch.uuid) + volcaFmPatchManagerMonth}
+                                        draggable={true}
+                                        onClick={() => makePatchPatchCurrent(patch.uuid)}
+                                        key={patch.uuid}>{patch.patch_name}</p>
+                                )}
+                            </div>
+                            <button className={'volcaFmPatchManagerUpdateButton' + volcaFmPatchManagerMonth}
+                                onClick={() => updateUserPatches()}>update</button>
+                        </div>
+                    </div>
                     <div className={'volcaFmPatchManagerPatchActionsDiv' + volcaFmPatchManagerMonth}></div>
-                    <div className={'volcaFmPatchManagerBanksDiv' + volcaFmPatchManagerMonth}></div>
+                    <div className={'volcaFmPatchManagerBanksDiv' + volcaFmPatchManagerMonth}>
+                        <div className={'volcaFmPatchManagerBanksContainer' + volcaFmPatchManagerMonth}>
+                            <div className={'volcaFmPatchManagerBanksBar' + volcaFmPatchManagerMonth}>
+                                <p className={'volcaFmPatchManagerBanksLabel' + volcaFmPatchManagerMonth}>banks:</p>
+                                <select className={'volcaFmPatchManagerBanksSelect' + volcaFmPatchManagerMonth}
+                                    onChange={(e) => changeBankEdit(e.target.value)}
+                                    value={currentEditBank}>
+                                    {patchCollection.banks.map(item => 
+                                        <option key={item.uuid} value={item.uuid}>{item.name}</option>
+                                    )}
+                                </select>
+                                <button className={'volcaFmPatchManagerAddBankButton' + volcaFmPatchManagerMonth}
+                                    onClick={() => addNewBank()}>add</button>
+                                {(patchCollection.banks.length > 1) && (
+                                    <button className={'volcaFmPatchManagerAddBankButton' + volcaFmPatchManagerMonth}
+                                        onClick={() => deleteCurrentBank()}>delete</button>
+                                )}
+                            </div>
+                            <div className={'volcaFmPatchManagerBankDisplayContainer' + volcaFmPatchManagerMonth}>
+                                {patchCollection.banks.map(item => 
+                                    <div className={'volcaFMPatchManagerBankDisplayer' + (item.uuid === currentEditBank) + volcaFmPatchManagerMonth}
+                                        onClick={() => {if (item.uuid !== currentEditBank) changeBankEdit(item.uuid)}}
+                                        key={item.uuid}></div>
+                                )}
+                            </div>
+                            <input className={'volcaFmPatchManagerBankNameInput' + volcaFmPatchManagerMonth}
+                                onChange={(e) => updateBankName(e.target.value)}
+                                type="text"
+                                value={getCurrentBankName()}/>
+                        </div>
+                    </div>
                     <div className={'volcaFmPatchManagerDeviceDiv' + volcaFmPatchManagerMonth}></div>
                     <div className={'volcaFmPatchManagerUserNotesDiv' + volcaFmPatchManagerMonth}></div>
                 </div>
