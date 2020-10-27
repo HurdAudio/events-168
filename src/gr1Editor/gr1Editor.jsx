7661d3b050ebfdf12881494827f28dbff0200033
hurdaudio <hurdaudio@gmail.com>
2020-10-26 19:12:39 -0600

    tasty chips gr-1 editor - stage 5 - february a

--- src/gr1Editor/gr1Editor.jsx
+++ src/gr1Editor/gr1Editor.jsx
@@ -13,6 +13,7 @@ import sampleImage from '../img/sampleImage.png';
 import './gr1Editor.style.jana.css';
 import './gr1Editor.style.janb.css';
 import './gr1Editor.style.janc.css';
+import './gr1Editor.style.feba.css';
 import axios from 'axios';
 import midi5pin from '../img/midi5pin.svg';
 import midiConnection from '../midiManager/midiConnection';
@@ -22,6 +23,7 @@ let connections;
 const januaryASpinner = 'https://events-168-hurdaudio.s3.amazonaws.com/gr1-editor/spinners/unnamed_GR1_spinner.gif';
 const januaryBSpinner = 'https://events-168-hurdaudio.s3.amazonaws.com/gr1-editor/spinners/loading51.gif';
 const januaryCSpinner = 'https://events-168-hurdaudio.s3.amazonaws.com/gr1-editor/spinners/JVQ25yB.gif';
+const februaryASpinner = 'https://events-168-hurdaudio.s3.amazonaws.com/gr1-editor/spinners/GratefulTornDavidstiger-max-1mb.gif';
 
 function Gr1Editor(user, incomingPatch) {
         
@@ -48,7 +50,7 @@ function Gr1Editor(user, incomingPatch) {
     const [userPatches, setUserPatches] = useState([]);
     const [saveAsDialogStatus, setSaveAsDialogStatus] = useState('_Inactive');
     const [aboutGR1DivState, setAboutGR1DivState] = useState('_Inactive');
-    const [currentSpinner, setCurrentSpinner] = useState(januaryCSpinner);
+    const [currentSpinner, setCurrentSpinner] = useState(februaryASpinner);
     const [saveAsName, setSaveAsName] = useState('');
     const [currentOutput, setCurrentOutput] = useState(null);
     const [midiConnections, setMidiConnections] = useState(undefined);
@@ -56,7 +58,7 @@ function Gr1Editor(user, incomingPatch) {
     const [availableInputs, setAvailableInputs] = useState([]);
     const [availableOutputs, setAvailableOutputs] = useState([]);
     const [currentMidiChannel, setCurrentMidiChannel] = useState(0);
-    const [gr1Month, setGr1Month] = useState('_JanuaryC');
+    const [gr1Month, setGr1Month] = useState('_FebruaryA');
     const [midiImage, setMidiImage] = useState(midi5pin);
     const [patchAltered, setPatchAltered] = useState(false);
     const [globalParams, setGlobalParams] =  useState({
